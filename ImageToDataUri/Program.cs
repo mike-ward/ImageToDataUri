bc015d1f6a0f6c5363a9863168f48a8c84b85623
mike-ward <mike@mike-ward.net>
2016-05-30 11:48:39 -0400

    intial commit

---
+++ ImageToDataUri/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace ImageToDataUri
+{
+    internal class Program
+    {
+        private static int Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.Error.WriteLine("usage: ImageToDataUri <file> <format>");
+                return 1;
+            }
+
+            var dataUri = $"data:image/{args[1]};base64,"
+                          + Convert.ToBase64String(File.ReadAllBytes(args[0]));
+
+            Console.Write(dataUri);
+            return 0;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# ImageToDataUri
-Because I couldn't find one to download
+ImageToDataUri
+==============
+
+Because I couldn't find one to download.
+
+    usage: ImageToDataUri file format
+
+    format = png, jpg, etc.
+
+The main advantage here is that it can handle large image sprites which
+many online converters do not.
